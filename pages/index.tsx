806d07a65e3c63537b74841e86f6e6bb64d5dcd3
Dawid Miczek <d.miczek@ideo.pl>
2021-05-23 19:35:20 +0200

    feat: greeting

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,19 @@ import s from './index.module.scss'
 
 const Home = () => {
   return (
-    <div className={s.greeting}>
-      cześć1
+    <div className={s.container}>
+      <div className={s.greeting}>
+        <div className={s.greetingHeading}>
+          Cześć, jestem Dawid
+        </div>
+        <div className={s.greetingSubHeading}>
+          a to mój blog o <span className={s.gradientPrimary}>programowaniu</span>.<br />
+          Mam nadzieje że znajdziesz coś dla siebie<br />
+          i zostaniesz na <span className={s.gradientSecondary}>dłużej</span>.
+        </div>
+      </div>
+
+
     </div>
   )
 }
